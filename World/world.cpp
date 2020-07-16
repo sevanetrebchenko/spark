212ecba548d2dbe08a598e8ca77ad6b6de55d782
Tristan Bouchard <tristan.bouchard@digipen.edu>
2020-07-15 18:44:02 -0800

    Reformatted file for better readability
    Commented code

--- World/world.cpp
+++ World/world.cpp
@@ -1,17 +1,19 @@
 #include "world.h"
 
-namespace ENGINE_NAME {
+namespace ENGINE_NAME 
+{
     World* World::_world = nullptr;
 
-    ECS::Entities::EntityManager *World::GetEntityManager() {
+    ECS::Entities::EntityManager *World::GetEntityManager() 
+    {
         return &_entityManager;
     }
 
-    World &World::GetInstance() {
-        if (!_world) {
+    // Gets the instance
+    World &World::GetInstance() 
+    {
+        if (!_world)
             _world = new World();
-        }
-
         return *_world;
     }
 }
